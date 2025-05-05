b089021ae208dbf172009326c62ad69f04b7ab9d
Oscar <oscar.pusniak@telecom-paris.fr>
2025-05-05 16:45:50 +0200

    oui

---
+++ src/robotsim/view/BasicStroke.java
@@ -0,0 +1,43 @@
+
+
+public class BasicStroke implements Stroke
+{
+    private Color color;
+    private float thickness;
+    private float[] dashPattern;
+
+    public static final Stroke TRAITPLEINFIN = new BasicStroke();
+
+    public static final Stroke TRAITPOINTILLEFIN = new BasicStroke(BLACK, 0.1, [0.1, 0.1]);
+
+    public static final Stroke TRAITPLEINEPAIS = new BasicStroke(BLACK, 0.5, []);
+
+
+    public BasicStroke()
+    {
+        this(BLACK, 0.1, []);
+    }
+
+    public BasicStroke(Color color, float thickness, float[] dashPattern)
+    {
+        this.color = color;
+        this.thickness = thickness;
+        this.dashPattern = dashPattern;
+    }
+
+    public Color getColor()
+    {
+        return color;
+    }
+
+    public float getThickness()
+    {
+        return thickness;
+    }
+
+    public float getDashPattern()
+    {
+        return dashPattern;
+    }
+
+}
