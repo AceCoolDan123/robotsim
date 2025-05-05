86e1146884fcce82c73fe5a7b32f82b7d993e0ba
Dadidou <luodaniel2015@gmail.com>
2025-05-05 14:36:35 +0200

    working in main

--- src/robotsim/Main.java
+++ src/robotsim/Main.java
@@ -13,26 +13,29 @@ import robotsim.model.Room;
 public class Main {
     public static void main(String[] args) 
     {
-        int factoryLength = 100;
-        int robotLength = 100;
-        Point point00 = createPoint(0, 0);
+        int factoryLength = 1000;
+        int roomLength = 300;
+        int robotLength = 10;
+        Point centerPoint = createPoint(0, 0);
+        Point leftPoint = createPoint(-500, 0);
+        Point rightPoint = createPoint(500, 0);
 
 
         Room[] rooms = new Room[3];
-        rooms[0] = new Room(point00, createDimension(point00, length / 2, length / 2), createSingleArrayDoors("DoorRoom0"), createSingleArrayAreaWithOneMachine("AreaRoom0"), "Room0");
-        //rooms[1] = new Room(point00, dim00, createSingleArrayDoors("DoorRoom1"), createSingleArrayAreaWithOneMachine("AreaRoom1"), "Room1");
-        //rooms[2] = new Room(point00, dim00, createSingleArrayDoors("DoorRoom2"), createSingleArrayAreaWithOneMachine("AreaRoom2"), "Room2");
+        rooms[0] = new Room(leftPoint, createDimension(leftPoint, roomLength, roomLength), createSingleArrayDoors("DoorRoom0"), createSingleArrayAreaWithOneMachine("AreaRoom0"), "Room0");
+        rooms[1] = new Room(centerPoint, createDimension(centerPoint, roomLength, roomLength), createSingleArrayDoors("DoorRoom1"), createSingleArrayAreaWithOneMachine("AreaRoom1"), "Room1");
+        rooms[2] = new Room(rightPoint, createDimension(rightPoint, roomLength, roomLength), createSingleArrayDoors("DoorRoom2"), createSingleArrayAreaWithOneMachine("AreaRoom2"), "Room2");
 
         Factory factory1 = new Factory(
             new Point(0, 0), 
-            createDimension(point00, length, length),
+            createDimension(centerPoint, factoryLength, factoryLength),
             "Factory 1", rooms, 
             createSingleArrayStations("station0"), 
             createSingleArrayPuck("puck0"));
 
-        factory1.addRobot(point00, createDimension(point00, 10, 10), "Robot0");
-        factory1.addRobot(point00, new Dimension(point00, point00, point00, point00), "Robot1");
-        factory1.addRobot(point00, new Dimension(point00, point00, point00, point00), "Robot2");
+        factory1.addRobot(leftPoint, createDimension(leftPoint, robotLength, robotLength), "Robot0");
+        factory1.addRobot(centerPoint, createDimension(centerPoint, robotLength, robotLength), "Robot0");
+        factory1.addRobot(rightPoint, createDimension(rightPoint, robotLength, robotLength), "Robot0");
 
         System.out.println(factory1);
     }
@@ -44,48 +47,48 @@ public class Main {
 
     private static Door[] createSingleArrayDoors(String name)
     {
-        Point point00 = new Point(0, 0);
+        Point centerPoint = new Point(0, 0);
         Door[] res = new Door[1];
-        res[0] = new Door(point00, point00, name);
+        res[0] = new Door(centerPoint, centerPoint, name);
         return res;
     }
 
     private static Area[] createSingleArrayAreaWithOneMachine(String name)
     {
-        Point point00 = new Point(0, 0);
-        Dimension dim00 = new Dimension(point00, point00, point00, point00);
+        Point centerPoint = new Point(0, 0);
+        Dimension dim00 = new Dimension(centerPoint, centerPoint, centerPoint, centerPoint);
         Area[] res = new Area[1];
         Machine[] machines = new Machine[1];
-        machines[0] = new Machine(point00, dim00, "name's Machine");
-        res[0] = new Area(point00, dim00, machines, name);
+        machines[0] = new Machine(centerPoint, dim00, "name's Machine");
+        res[0] = new Area(centerPoint, dim00, machines, name);
         return res;
     }
 
     private static ChargingStation[] createSingleArrayStations(String name)
     {
-        Point point00 = new Point(0, 0);
-        Dimension dim00 = new Dimension(point00, point00, point00, point00);
+        Point centerPoint = new Point(0, 0);
+        Dimension dim00 = new Dimension(centerPoint, centerPoint, centerPoint, centerPoint);
         ChargingStation[] res = new ChargingStation[1];
-        res[0] = new ChargingStation(point00, dim00, name);
+        res[0] = new ChargingStation(centerPoint, dim00, name);
         return res;
     }
 
     private static Puck[] createSingleArrayPuck(String name)
     {
-        Point point00 = new Point(0, 0);
-        Dimension dim00 = new Dimension(point00, point00, point00, point00);
+        Point centerPoint = new Point(0, 0);
+        Dimension dim00 = new Dimension(centerPoint, centerPoint, centerPoint, centerPoint);
         Puck[] res = new Puck[1];
-        res[0] = new Puck(point00, dim00, name);
+        res[0] = new Puck(centerPoint, dim00, name);
         return res;
     }
 
     private static Dimension createDimension(Point center, int height, int width)
     {
         return new Dimension(
-            new Point(center.x - width, center.y + height), 
-            new Point(center.x + width, center.y + height), 
-            new Point(center.x + width, center.y - height), 
-            new Point(center.x - width, center.y - heigth)
+            new Point(center.x - width / 2, center.y + height / 2), 
+            new Point(center.x + width / 2, center.y + height / 2), 
+            new Point(center.x + width / 2, center.y - height / 2), 
+            new Point(center.x - width / 2, center.y - height / 2)
         );
     }
 }
