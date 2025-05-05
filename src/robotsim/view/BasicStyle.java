2bbeea8e08ef3d9021343b4e2c7ada992fc33336
Oscar <oscar.pusniak@telecom-paris.fr>
2025-05-05 16:45:15 +0200

    default styles

--- src/robotsim/view/BasicStyle.java
+++ src/robotsim/view/BasicStyle.java
@@ -5,7 +5,17 @@ public class BasicStyle implements Style
     private Color backgroundColor;
     private Stroke stroke;
 
-    public static final 
+    public static final Style DefaultRobot = BasicStyle(PINK, TRAITPLEINFIN);
+
+    public static final Style DefaultRoom = BasicStyle(WHITE, TRAITPLEINFIN);
+
+    public static final Style DefaultArea = BasicStyle(WHITE, TRAITPOINTILLEFIN);
+
+    public static final Style DefaultOpenedDoor = BasicStyle(WHITE, TRAITPLEINEPAIS);
+    
+    public static final Style DefaultClosedDoor = BasicStyle(BLACK, TRAITPLEINEPAIS);
+
+    
 
     public BasicStyle()
     {
@@ -16,6 +26,11 @@ public class BasicStyle implements Style
     {
         this.backgroundColor = backgroundColor;
         this.stroke = stroke;
+    }
+
+    public void setBackgroundColor(color backgroundColor)
+    {
+        this.backgroundColor = backgroundColor;
     }
 
     public Color getBackgroundColor()
